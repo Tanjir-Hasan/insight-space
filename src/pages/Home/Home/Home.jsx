df7df1c34902486a92c6080f0bbe15e373fdef93
SumaiyaAK <sumaiyasrn6@gmail.com>
2023-08-15 23:53:37 +0600

    peer to peer component added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "../Banner/Banner";
 import ChoiceUs from "../ChoiceUs/ChoiceUs";
 import CommunityGuidelines from "../CommunityGuidelines/CommunityGuidelines";
 import ExploreContent from "../Explore Content/ExploreContent";
+import PeerToPeer from "../PeerToPeer/PeerToPeer";
 
 import TopPosts from "../TopPosts/TopPosts";
 
@@ -13,6 +14,7 @@ const Home = () => {
             <TopPosts></TopPosts>
             <ExploreContent></ExploreContent>
             <ChoiceUs></ChoiceUs>
+            <PeerToPeer></PeerToPeer>
             <CommunityGuidelines></CommunityGuidelines>
         </> )
 };
