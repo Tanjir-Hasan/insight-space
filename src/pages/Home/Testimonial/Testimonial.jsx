45002af600377cfd5d4bad27db3765f4f0eedbe7
Tanjir-Hasan <ihasan.tanjir@gmail.com>
2023-08-24 16:26:13 +0600

    testimonial responsive and animation added done

--- src/pages/Home/Testimonial/Testimonial.jsx
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -2,7 +2,19 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import { useContext } from 'react';
+import { ThemeContext } from '../../../providers/ThemeProvider';
+import { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
 const Testimonial = () => {
+
+    const { theme } = useContext(ThemeContext);
+
+    const controls = useAnimation();
+
+    const [ref, inView] = useInView();
 
     const { data: testimonials = [] } = useQuery({
         queryKey: ['testimonials'],
@@ -32,61 +44,80 @@ const Testimonial = () => {
             slidesToSlide: 1
         }
     };
-    return (
-        <div>
-            <div className="md:w-10/12 w-11/12 mx-auto">
 
-                <h1 className='text-center md:text-5xl text-4xl font-[Poppins] border-b-2 border-[#84a98c] w-1/2 mx-auto lg:pt-20 pt-10 '>
-                    Testimonials
-                    <br />
+
+    useEffect(() => {
+        if (inView) {
+            controls.start('visible');
+        } else {
+            controls.start("hidden");
+        }
+    }, [controls, inView]);
+
+    return (
+        <div className={`${theme === 'dark' ? 'dark' : ''} pb-10`}>
+
+            <motion.div
+                ref={ref}
+                initial="hidden"
+                animate={controls}
+                variants={{
+                    visible: { opacity: 1, x: 0 },
+                    hidden: { opacity: 0, x: 100 },
+                }}
+                transition={{ duration: 0.9 }}
+
+                className='md:w-10/12 w-11/12 mx-auto '>
+
+                <h1 className='md:text-5xl text-4xl flex md:flex-row flex-col items-baseline gap-3 font-[Poppins] border-b-2 border-[#84a98c] md:w-5/12 lg:pt-20 pt-10 '>
+                    <span>Testimonials</span>
                     <span className='text-xl'>- What's says our users -</span>
                 </h1>
 
-            </div>
+            </motion.div>
 
-            <div className="">
-                <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div className="kakon">
-                        <Carousel
-                            responsive={responsive}
-                            swipeable
-                            draggable
-                            arrows={true}
-                            showDots={true}
-                            infinite={true}
-                            autoPlay={true}
-                            autoPlaySpeed={3000}
-                            keyBoardControl
-                            containerClass="carousel-container"
-                            dotListClass="custom-dot-list-style"
-                        >
-                            {testimonials?.map((testimonial) => (
-                                <div key={testimonial?._id} className="bg-white sm:rounded-lg p-6 h-48 my-10 hover:bg-[#84a98c] duration-700 shadow-xl m-3 overflow-hidden">
-                                    <div className="flex items-center mb-4">
-                                        <img className="w-10 h-10 rounded-full mr-4" src={testimonial?.photo} alt={testimonial?.userName} />
-                                        <div>
-                                            <h3 className=" capitalize text-lg font-medium text-gray-900">{testimonial?.userName}</h3>
-                                            <div className="mt-1 flex items-center">
-                                                {[...Array(parseInt(testimonial?.rating))].map((_, index) => (
-                                                    <svg key={index} className="h-4 w-4 fill-current text-yellow-500" viewBox="0 0 20 20">
-                                                        <path
-                                                            fillRule="evenodd"
-                                                            d="M10 1l2.928 6.856 6.072.472-4.664 4.38 1.388 6.08-5.824-3.512L4.376 18.76l1.388-6.08L0 8.328l6.072-.472L10 1z"
-                                                        />
-                                                    </svg>
-                                                ))}
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <p className="text-sm ">{testimonial?.message}</p>
+            <div className="md:w-9/12 w-11/12 mx-auto px-4 sm:px-6 lg:px-8">
+
+                <Carousel
+                    responsive={responsive}
+                    swipeable
+                    draggable
+                    arrows={false}
+                    showDots={true}
+                    infinite={true}
+                    autoPlay={true}
+                    autoPlaySpeed={3000}
+                    keyBoardControl
+                    containerClass="carousel-container"
+                    dotListClass="custom-dot-list-style"
+                >
+                    {testimonials?.map((testimonial) => (
+                        <div key={testimonial?._id} className="bg-white text-black sm:rounded-lg p-6 h-48 my-10 shadow-xl m-3 overflow-hidden">
+                            <div className="flex items-center mb-4">
+                                <img className="w-10 h-10 rounded-full mr-4" src={testimonial?.photo} alt={testimonial?.userName} />
+                                <div>
+                                    <h3 className=" capitalize text-lg font-medium text-gray-900 font-[Cinzel]">{testimonial?.userName}</h3>
+                                    <div className="mt-1 flex items-center">
+                                        {[...Array(parseInt(testimonial?.rating))].map((_, index) => (
+                                            <svg key={index} className="h-4 w-4 fill-current text-yellow-500" viewBox="0 0 20 20">
+                                                <path
+                                                    fillRule="evenodd"
+                                                    d="M10 1l2.928 6.856 6.072.472-4.664 4.38 1.388 6.08-5.824-3.512L4.376 18.76l1.388-6.08L0 8.328l6.072-.472L10 1z"
+                                                />
+                                            </svg>
+                                        ))}
                                     </div>
                                 </div>
-                            ))}
-                        </Carousel>
-                    </div>
-                </div>
+                            </div>
+                            <div>
+                                <p className="text-sm font-[Cinzel]">{testimonial?.message}</p>
+                            </div>
+                        </div>
+                    ))}
+                </Carousel>
+
             </div>
+
         </div>
     );
 };
